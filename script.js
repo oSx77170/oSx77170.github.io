e7f928d50de1c192e81d56e0750435de03e316ca
oSx77170 <alexandre.monney@laposte.net>
2020-05-05 19:24:39 +0200

    troll

---
+++ script.js
@@ -0,0 +1,12 @@
+let button = document.querySelector('.ban-button');
+
+
+button.addEventListener('click', function(){
+    document.querySelector('#site').innerText = "MANU TES MOCHE";
+    document.querySelector('#site').style.display = 'flex';
+    document.querySelector('#site').style.paddingTop = "400px";
+    document.querySelector('#site').style.justifyContent = 'center';
+    document.querySelector('#site').style.margin = 'auto';
+    document.querySelector('#site').style.heigth = '1980px';
+    
+})
